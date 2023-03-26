3455a63890339a5213eef450c9a659808bc15bb7
AdityaVed95 <adityaved95@gmail.com>
2023-03-26 12:36:16 +0530

    creating forms and accepting user response and updating database (json file) and applying base.html for common css on all templates

--- model.py
+++ model.py
@@ -16,5 +16,10 @@ def load_db():
     with open("flashcards_db.json") as f:
         return json.load(f)
 
-
 db = load_db()
+
+def save_db():
+    with open("flashcards_db.json",'w') as f:
+        return json.dump(db,f)    
+
+
